8ca1b88840119ae2473271e7df5753595c42bbc1
Isaac <isaacgeazy@gmail.com>
2021-04-13 11:26:50 -0300

    Negativo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Decisoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+
+            Console.Write("Digite um número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if(n1 < 0){ 
+
+                Console.Write("Voce digitou um número negativo!");
+
+
+            }
+        }
+    }
+}
